470b689cd549c16c47a44e7dd260a1bf4a3acc31
leoworm <leo.charles.p@gmail.com>
2017-11-16 13:01:45 +0100

    switched to tiles

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		boolean isGameRunning = true;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		while(isGameRunning) {
+			Tile test = new Tile();
+			
+		}
+		sc.close();
+	}
+	public static createMap
+
+}
---
+++ src/Tile.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Tile {
+	String type = "";
+	int connections = 0;
+	
+	
+	public void editTile(String type, Integer connections) {
+		rooms.add(this);
+	}
+}
