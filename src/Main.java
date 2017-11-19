2044ca61b1116325ba2b814cab304e0a46585230
leoworm <leo.charles.p@gmail.com>
2017-11-19 21:47:04 +0100

    Added basic map rendering

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import java.util.*;
 import java.io.*;
+import javax.swing.*;
+import java.awt.GridLayout;
+
+//TODO: add a scale argument to the toAscii method
 
 public class Main {
 
 	public static void main(String[] args) {
 		boolean isGameRunning = true;
-		
+		drawMap(parseMap("map1.map"));
+		//System.exit(0);
 
-		System.out.println(parseMap("map1.map").get(0).get(3));
 		
 
 	}
@@ -36,5 +40,47 @@ public class Main {
 			ex.printStackTrace();
 		}
 		return map;
+	}
+	
+	public static String getRowContent(List<List<String>> map, Integer row) {
+		String output = "";
+		for(int i = 0; i < map.get(row).size(); i++) {
+			output = output + map.get(row).get(i);
+		}
+		return output;
+	}
+	
+	public static void drawMap(List<List<String>> map) {
+
+		JPanel panel = new JPanel(new GridLayout(map.size(), map.get(0).size(), 0, 0));
+
+		for(int i = 0; i < map.size(); i++) {
+			for(int j = 0; j < map.get(i).size(); j++) {
+				char current = toAscii(map, i).charAt(j);
+				panel.add(new JLabel(Character.toString(current)));
+			}
+		}
+		
+		JFrame frame = new JFrame();
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static String toAscii(List<List<String>> map, Integer row) {
+		String result = "";
+		for(int i = 0; i < map.get(row).size(); i++) {
+			String current = map.get(row).get(i);
+			switch(current) {
+			case "wall":
+				result = result + "#";
+				break;
+			case "floor":
+				result = result + "·";
+				break;
+				
+			}
+		}
+		return result;
 	}
 }
